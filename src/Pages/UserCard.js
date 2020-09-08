87afb58e2b889bc4f67a9a3b9a2209a83fe81545
sachin singh <48687519+sachin3018@users.noreply.github.com>
2020-09-08 22:26:04 +0530

    Update UserCard.js

--- src/Pages/UserCard.js
+++ src/Pages/UserCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardBody,Row, Col } from "reactstrap";
+import {  CardBody,Row, Col } from "reactstrap";
 import "../App.css"
 
 const UserCard = ({ user }) => {
